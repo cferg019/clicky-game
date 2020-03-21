e68591f4e2cd39c0ed9d79f1f2761402b00c74b8
Casey Ferguson <cferg019@gmail.com>
2020-03-20 20:15:32 -0400

    Updates

---
+++ precache-manifest.91d3136ca09c243ddbb4d57cee1ee1f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c628a432768ba1a6ed19e64f6006334",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "dca67f851a4359fdeaba",
+    "url": "/clicky-game/static/css/main.5bd452df.chunk.css"
+  },
+  {
+    "revision": "e842759b9f83a0fbcbc8",
+    "url": "/clicky-game/static/js/2.d5419ba6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.d5419ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca67f851a4359fdeaba",
+    "url": "/clicky-game/static/js/main.547ad7dc.chunk.js"
+  },
+  {
+    "revision": "c81afc240fbac3d8aeb5",
+    "url": "/clicky-game/static/js/runtime-main.4e8dbddb.js"
+  }
+]);
